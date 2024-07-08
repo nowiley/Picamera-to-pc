ceb2b0bb2c8cfa91653b3a13b448845335fef36c
njwiley <noahjwiley22@gmail.com>
2024-07-08 13:05:19 -0400

    Client and server work, stream images from pi to pc

--- client.py
+++ client.py
@@ -7,40 +7,30 @@ import cv2
 import socket
 import numpy as np
 
-host_ip = "192.168.50.36"
-host_port = 8000
-
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = (host_ip, host_port)
-print('connecting to %s port %s' % server_address)
+server_address = ('192.168.50.36', 8000)
 sock.connect(server_address)
 
-# Create a VideoCapture object and specify the video codec and port
-cap = cv2.VideoCapture('tcp://'+host_ip+':'+str(host_port), cv2.CAP_FFMPEG)
-
-if not cap.isOpened():
-    print('VideoCapture not opened')
-    exit(-1)
-
+# Receive the data in small chunks and reassemble it
+data = b""
 while True:
-    # Receive frame from the server
-    readsuccess, frame = cap.read()
+    data += sock.recv(4096)
+    start = 0
+    while True:
+        start = data.find(b'\xff\xd8', start)
+        end = data.find(b'\xff\xd9', start)
+        if start != -1 and end != -1:
+            jpg = data[start:end+2]
+            data = data[end+2:]
+            frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            cv2.imshow('Frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            start = 0
+        else:
+            break
 
-    if not readsuccess:
-        print('frame not read, frame empty')
-        break
-    
-    # Display the frame
-    cv2.imshow('frame', frame)
-
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the VideoCapture object and close the window
-cap.release()
 cv2.destroyAllWindows()
-
--- server.py
+++ server.py
@@ -7,13 +7,13 @@ FROM https://github.com/raspberrypi/picamera2/blob/main/examples/capture_stream.
 import socket
 import time
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration({"size": (640, 480)})
 picam2.configure(video_config)
-encoder = H264Encoder(1000000)
+encoder = JpegEncoder()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,12 +23,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     picam2.encoders = encoder
 
     conn, addr = sock.accept()
+    print("Connection from", addr)
     stream = conn.makefile("wb")
     encoder.output = FileOutput(stream)
     picam2.start_encoder()
     picam2.start()
-    time.sleep(20)
-    picam2.stop()
-    picam2.stop_encoder()
-    conn.close()
+    print("Picamera2 and encoder started")
+    try:
+        while True:  # Stream forever
+            time.sleep(1)  # Prevent the loop from running too fast
+    except KeyboardInterrupt:
+        print("Picamera2 and encoder stopped")
+        picam2.stop()
+        picam2.stop_encoder()
+        conn.close()
 
