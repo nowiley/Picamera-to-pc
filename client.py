a4096583387d25f110d65bb68671018d99b345a3
njwiley <noahjwiley22@gmail.com>
2024-07-08 13:26:47 -0400

    added ros publishing instructions

--- client.py
+++ client.py
@@ -6,26 +6,57 @@ FROM https://github.com/golubaca/python-streaming-server/blob/master/client/recv
 import cv2
 import socket
 import numpy as np
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+#####################
+# SETUP ROS AND TCP #
+#####################
+
+# Initialize the ROS node
+rospy.init_node('image_streamer')
+
+# Create a publisher for the image topic
+image_pub = rospy.Publisher("camera/image", Image, queue_size=10)
+
+# Create a CvBridge object for converting between OpenCV images and ROS image messages
+bridge = CvBridge()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.50.36', 8000)
+server_address = ('192.168.50.36', 8000) # IP, PORT
 sock.connect(server_address)
+
+
+######################
+# RECEIVE, SHOW, PUB #
+######################
+
 
 # Receive the data in small chunks and reassemble it
 data = b""
-while True:
+while not rospy.is_shutdown():
+    # Receive the data in small chunks and reassemble it
     data += sock.recv(4096)
     start = 0
     while True:
+        # Find start and end of the image
         start = data.find(b'\xff\xd8', start)
         end = data.find(b'\xff\xd9', start)
         if start != -1 and end != -1:
+            # Extract jpg and shift data
             jpg = data[start:end+2]
             data = data[end+2:]
+            # Decode jpg and display
             frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+            # Convert the OpenCV image to a ROS image message and publish it
+            image_msg = bridge.cv2_to_imgmsg(frame, "bgr8")
+            image_pub.publish(image_msg)
+
             cv2.imshow('Frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
